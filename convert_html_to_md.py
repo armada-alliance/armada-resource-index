34508cd864147b4d86a1e561f2723ee6b9804ac3
Wael <wael.t.ivie@gmail.com>
2021-11-30 17:46:47 -0800

    simple python script to convert html to md file

---
+++ convert_html_to_md.py
@@ -0,0 +1,17 @@
+# import packages
+import markdownify
+
+# read html file to convert
+html_file = open("bookmarks.html", "r")
+html_string = html_file.read()
+html_file.close()
+
+# convert html to markdown
+converted_html_md = markdownify.markdownify(html_string, heading_style='atx')
+
+# write markdown to file
+markdown_file = open('markdown_file.md', 'w')
+markdown_file.write(converted_html_md)
+markdown_file.close()
+
+
